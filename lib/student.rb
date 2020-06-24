c8e0e3ef670e930619b19d2452bae78884b7b63c
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 17:16:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
     # returns an array of all students in grades 11 or below
   end
 
-  def self.first_X_students_in_grade_10(students)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
       SELECT *
       FROM students
