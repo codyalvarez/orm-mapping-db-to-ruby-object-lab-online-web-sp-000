98667fd1d35cc73264594c89758e5265a5e1aba6
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 17:28:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,7 @@ class Student
       SQL
       DB[:conn].execute(sql).map do |row|
          self.new_from_db(row)
-      end.first[2]
+      end
     # returns all student instances from the db
   end
 
