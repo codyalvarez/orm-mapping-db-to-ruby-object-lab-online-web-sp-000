8b4b5ed74ed5d5682893deb6aee07cfca2a3dac0
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 16:59:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,27 +70,27 @@ class Student
     end
   end
 
-  # def self.students_below_12th_grade
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE grade <= 12
-  #   SQL
-  #   # returns an array of all students in grades 11 or below
-  # end
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade <= 12
+    SQL
+    # returns an array of all students in grades 11 or below
+  end
 
-  # def self.first_X_students_in_grade_10
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE GRADE = 10
-  #
-  #     SQL
-  #     DB[:conn].execute(sql, name).map dp |row|
-  #        self.new_from_db(row)
-  #     end.first
-  #   # returns all student instances from the db
-  # end
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE GRADE = 10
+  
+      SQL
+      DB[:conn].execute(sql, name).map dp |row|
+         self.new_from_db(row)
+      end.first
+    # returns all student instances from the db
+  end
 
   def self.first_student_in_grade_10
     # returns an array of the first X students in grade 10
