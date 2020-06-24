667e69f40a4e935c7c5de59248863a5da31dac6c
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 17:48:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,7 +101,7 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT ?
+      LIMIT 2
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
