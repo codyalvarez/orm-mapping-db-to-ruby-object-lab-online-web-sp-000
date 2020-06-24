3756941975640f74fadb332f58ebe65b87acd2b2
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 17:03:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
       FROM students
       WHERE grade <= 12
     SQL
-    DB:[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
     # returns an array of all students in grades 11 or below
